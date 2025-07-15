8aefbd90760c61633d7afdbd4f20f3603f9f7764
adrianenaws <168179925+adrianenaws@users.noreply.github.com>
2025-07-15 10:31:28 +0200

    Update streamlit_app.py
    Update indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,10 +48,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
     import requests
-    
+
     if ingredients_list:
-      ingredients_string = ''
+        ingredients_string = ''
+        
         for fruit_chosen in ingredients_list:
-          ingredients_string += fruit_chosen + ' '
-          smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-          sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+            ingredients_string += fruit_chosen + ' '
+            smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
