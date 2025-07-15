0fa979ed82edc9ecd1018bc6c0d03097b3292ee4
adrianenaws <168179925+adrianenaws@users.noreply.github.com>
2025-07-15 09:56:25 +0200

    Update streamlit_app.py
    Change website https://www.fruityvice.com/

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
