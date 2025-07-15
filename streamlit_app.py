d3dcf503f57fd69217a5352974abe499689a0f29
adrianenaws <168179925+adrianenaws@users.noreply.github.com>
2025-07-15 10:22:20 +0200

    Update streamlit_app.py
    repeat nutrition information for every fruit selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,11 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display nutritional information
 import requests
-smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+if ingredients_list:
+  ingredients_string = ''
+    for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
